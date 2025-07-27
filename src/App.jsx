0b44d1a435189c5be94b4750bcce3d7af9c27940
Ahmed Khaled <ahmedtalha40498@gmail.com>
2025-07-27 07:01:11 +0300

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 function App() {
   const queryClient = new QueryClient();
 
-  const route = createBrowserRouter([
+  const route = createHashRouter([
     {
       path: "",
       element: <Layout />,
